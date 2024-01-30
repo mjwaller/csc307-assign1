8c290408a968094b0ff559aa351b42a72df664cf
mjwaller <meganwaller04@gmail.com>
2024-01-29 19:57:12 -0800

    finished lab

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -28,7 +28,12 @@ const users = {
       id: "zap555",
       name: "Dennis",
       job: "Bartender"
-    }
+    },
+      {
+  "id": "qwe123",
+  "job": "Zookeeper",
+  "name": "Cindy"
+}
   ]
 };
 
@@ -41,6 +46,38 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
       users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+const deleteUserById = (id) => {
+  const index = users["users_list"].findIndex((user) => user["id"] === id);
+  if (index !== -1) {
+    const deletedUser = users["users_list"].splice(index, 1)[0];
+    return deletedUser;
+  } else {
+    return null;
+  }
+};
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  const deletedUser = deleteUserById(id);
+  if (deletedUser !== null) {
+    res.send(deletedUser);
+  } else {
+    res.status(404).send("Resource not found.");
+  }
+});
+
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
@@ -52,7 +89,8 @@ app.get("/users/:id", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-  const name = req.query.name;
+    const name = req.query.name;
+    const job = req.query.job;
   if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
